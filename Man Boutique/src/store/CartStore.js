79497fcc0ee6d59670c8b245609e281e1112f85d
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2023-12-18 16:01:28 +0000

    alert when product is added to cart. some comments

--- Man Boutique/src/store/CartStore.js
+++ Man Boutique/src/store/CartStore.js
@@ -1,76 +1,81 @@
-import { defineStore } from 'pinia';
-import { useProductStore } from './ProductStore';
+import { defineStore } from "pinia";
+import { useProductStore } from "./ProductStore";
 
 export const useCartStore = defineStore({
-  id: 'cartStore',
+  id: "cartStore",
   state: () => ({
     cart: [],
   }),
   getters: {
     totalQuantity() {
+      //return this.cart.length
       return this.cart.reduce((total, product) => total + product.quantity, 0);
     },
   },
   actions: {
     addToCart(product) {
-      const existingProduct = this.cart.find(item => item.id === product.id);
+      //verifica se produto já existe no carrinho
+      const existingProduct = this.cart.find((item) => item.id === product.id);
       const productStore = useProductStore();
+      let success = false;
 
-      //console.log('Product:', product);
-      //console.log('Existing Product:', existingProduct);
-      //console.log('Product Store:', productStore);
-
-
-    
+      //se não existir, adiciona ao carrinho
       if (!existingProduct) {
-        const productInStore = productStore.products.find(p => p.id === product.id);
-                                                                                      //console.log('Product in store:', productInStore);
-          if (!productInStore) {
-          console.error('Product not found in the store:', product.id);
+        const productInStore = productStore.products.find(
+          (p) => p.id === product.id
+        );
+        //se não existir na productStore, cancela
+        if (!productInStore) {
+          console.error("Product not found in the store:", product.id);
           this.saveCartToLocalStorage();
           return;
         }
-    
+
+        //verifica se tem stock disponível
         const availableStock = productInStore.quantity;
-                                                                                      //console.log('Available stock:', availableStock);    
-                                                                                      //console.log('Product in store:', productInStore.quantity);
-    
+        
         if (availableStock > 0) {
+          //se tiver stock adiciona ao carrinho
           this.cart.push({ ...productInStore, quantity: 1 });
-          console.log('Cart:', this.cart);
+          success = true;
         } else {
-                                                                                      //console.log('Not enough stock available.');
-          alert('Not enough stock available.');
+          //console.log('Not enough stock available.');
+          alert("Not enough stock available.");
         }
       } else {
-        const productInStore = productStore.products.find(p => p.id === product.id);
-        const availableStock = productInStore.quantity - existingProduct.quantity;
-    
+        //se já existir, incrementa quantidade
+        const productInStore = productStore.products.find(
+          (p) => p.id === product.id
+        );
+        const availableStock =
+          productInStore.quantity - existingProduct.quantity;
+
         if (availableStock > 0) {
           existingProduct.quantity += 1;
-                                                                                     // console.log('Cart:', this.cart);
+          success = true;
         } else {
-          console.log('Not enough stock available.');
-          alert('Not enough stock available.');
+          alert("Not enough stock available.");
         }
       }
       this.saveCartToLocalStorage();
+
+
+      return success;
     },
 
     saveCartToLocalStorage() {
-      localStorage.setItem('cart', JSON.stringify(this.cart));
+      localStorage.setItem("cart", JSON.stringify(this.cart));
     },
-    
 
     removeFromCart(productId) {
-      const index = this.cart.findIndex(item => item.id === productId);
+      const index = this.cart.findIndex((item) => item.id === productId);
       if (index !== -1) {
         this.cart.splice(index, 1);
       }
     },
 
     decrementQuantity(productId) {
-      const existingProduct = this.cart.find(item => item.id === productId);
+      const existingProduct = this.cart.find((item) => item.id === productId);
 
       if (existingProduct && existingProduct.quantity > 1) {
         existingProduct.quantity -= 1;
