08101fe617f1c2117f341f0e06beb09defd09c58
Ricardo Ferreira <ricardo.ferreira.t0123185@edu.atec.pt>
2023-12-24 17:46:52 +0000

    aplyed button style to contact page

--- Man Boutique/src/components/ProductCard.vue
+++ Man Boutique/src/components/ProductCard.vue
@@ -103,7 +103,7 @@ const addToCartHandler = (productId) => {
 };
 </script>
 
-<style  >
+<style  scoped>
 .card-img-top {
   object-fit: cover;
   padding: 2rem;
@@ -144,7 +144,23 @@ const addToCartHandler = (productId) => {
   height: 8rem;
 }
 
+.btn {
+    background-color: #171133;
+    border-color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
+    font-weight: bold;
+    transition: all 0.5s ease-in-out;
+}
 
+
+.btn:hover {
+    background-color: #171133;
+    border-color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
+    font-weight: bold;
+    transform: scale(1.05);
+    transition: all 0.5s ease-in-out;
+}
 
 
 </style>
